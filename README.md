880305124e37a5dde1bcd639945e59add76a6334
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-03-05 15:18:06 +0530

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# azure-apim
-#testing the readme
--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "apim_gateway_url" {
-  description = "The gateway URL of the API Management instance."
-  value       = azurerm_api_management.apim.gateway_url
-}
-
-output "hello_api_url" {
-  description = "The URL of the published Hello API. Append '/hello' to test."
-  value       = "${azurerm_api_management.apim.gateway_url}/hello"
-}
