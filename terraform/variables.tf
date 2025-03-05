739b8ed46ce14c816a03e390e0c2f66fa1f4275f
Akashdpakash <54543615+Akashdpakash@users.noreply.github.com>
2025-03-05 17:57:51 +0530

    updating variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,24 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
+variable "apim_name" {
+  description = "Name of the API Management instance"
   type        = string
 }
 
-variable "tenant_id" {
-  description = "Azure Tenant ID"
+variable "location" {
+  description = "Azure region where resources will be deployed"
   type        = string
 }
 
-variable "client_id" {
-  description = "Azure Client ID"
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "publisher_name" {
+  description = "Name of the API publisher"
+  type        = string
+}
+
+variable "publisher_email" {
+  description = "Email of the API publisher"
   type        = string
 }
